2fee203817742db467cc59e3ed2bb94ce846a9f8
Hacker001uzb <156788115+Hacker001uzb@users.noreply.github.com>
2024-02-12 06:04:17 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var a = prompt('How old are you:')
+    if (a == 18){
+        console.log('Equal')
+    }
+    else if (a < 18){
+        console.log('Middle brothe')
+    }
+    else if (a >18 ){
+        console.log('Brother')
+    }
